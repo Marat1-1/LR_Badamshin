40455a8e048ad09e391ccd58cf33490afc17eb32
Bad_Marat <badamshin1975@yandex.ru>
2021-09-14 12:47:03 +0300

    commit1

---
+++ Badamshin_newLR/Badamshin_newLR/Badamshin_newLR.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+
+struct Pipe
+{
+	int id;
+	int diameter;
+	double length;
+	bool repair;
+};
+
+struct CompressorStation
+{
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Pipe p;
+	p.id = 0;
+	cout << "Введи диаметр трубы: ";
+	cin >> p.diameter;
+	cout << "id - " << p.id << endl
+		<< "диаметр - " << p.diameter;
+}
